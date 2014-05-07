3d4b6add6c167ae35292d529ba7881cb82c098f9
Marco Tulio Ribeiro <marcotcr@gmail.com>
2014-05-07 11:24:15 -0700

    Added construction of biego networks

---
+++ children/construct-biego-networks.py
@@ -0,0 +1,32 @@
+import argparse
+import collections
+
+def main():
+  parser = argparse.ArgumentParser(description='Generate bi-ego networks. Writes files that are named node1-node2.edges')
+  parser.add_argument('-o', required=True,  dest='output_folder', help='output folder')
+  parser.add_argument('-e', required=True, dest='edges', help='edge file')
+  parser.add_argument('-n', required=True, dest='nodes', help='node attribute file')
+  args = parser.parse_args()
+  node_gender = {}
+  for line in open(args.nodes):
+    node, gender = line.split()[0], line.strip().split()[2]
+    if gender == 'M' or gender == 'F':
+      node_gender[node] = gender
+  neighbors = collections.defaultdict(lambda: set())
+  for line in open(args.edges):
+    node1, node2 = line.strip().split()
+    neighbors[node1].add(node2)      
+    neighbors[node2].add(node1)      
+  for line in open(args.edges):
+    node1, node2 = line.strip().split()
+    if node1 in node_gender and node2 in node_gender:
+      output = open('%s/%s-%s.edges' % (args.output_folder, node1, node2), 'w')
+      all_nodes = neighbors[node1].union(neighbors[node2])
+      for node in all_nodes:
+        for neb in neighbors[node]:
+          if neb in all_nodes:
+            output.write('%s %s\n' % (node, neb))
+      output.close()
+
+if __name__ == '__main__':
+  main()
